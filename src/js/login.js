f2084c203cd02d4af368750b824f940dcc241c03
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-08-09 14:27:32 -0500

    Subiendo estructura de vistas principales

---
+++ src/js/login.js
@@ -0,0 +1,10 @@
+initializeFirebase();
+
+document.getElementById('adminLogin').addEventListener('click', event => {
+  event.preventDefault();
+  adminLogIn();
+});
+document.getElementById('newAdmin').addEventListener('click', event => {
+  event.preventDefault();
+  newAdminForm();
+});
