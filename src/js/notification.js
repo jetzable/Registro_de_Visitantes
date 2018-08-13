634f6ef2052c5ef789eaee5afea02b021fa8214a
DenyMM <deny.lolipop@gmail.com>
2018-08-13 09:37:15 -0500

    se agrego funcionalidad de notificaciones

---
+++ src/js/notification.js
@@ -0,0 +1,22 @@
+// codigo del email
+
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+  var template_id = "users_visitapp";
+
+
+  myform.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,"myform")
+  	.then(function(){
+    	alert("Sent!");
+       myform.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+  return false;
+});
